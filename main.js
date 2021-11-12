32e9a9079e442eecbf0ebaa2d39981000789ad96
Saurabh Kumar <asksaurabh101@gmail.com>
2021-11-12 14:28:39 +0530

    Add event listeners to button

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 const Phrase = require("harbinger-complex-palindrome-detector");
 
-const userInput = prompt("Enter a string to check palindromicity: ", "abba");
-let phrase = new Phrase(userInput);
+function palindromeTester() {
+    const userInput = prompt("Enter a string to check palindromicity: ", "abba");
+    let phrase = new Phrase(userInput);
 
-if(!!userInput  && phrase.isPalindrome() === true) {
-    alert(`"${phrase.content}" is a palindrome!!`);    
-} else {
-    alert(`"${phrase.content}" is not a palindrome!!`)
+    if(!!userInput  && phrase.isPalindrome() === true) {
+        alert(`"${phrase.content}" is a palindrome!!`);    
+    } else {
+        alert(`"${phrase.content}" is not a palindrome!!`)
+    }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    let button = document.querySelector("#palindromeTester");
+    button.addEventListener("click", function () {
+        palindromeTester();
+    });
+});
