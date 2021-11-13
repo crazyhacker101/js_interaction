4874f466169f9f6bb38c8620619cb879312044c4
Saurabh Kumar <asksaurabh101@gmail.com>
2021-11-13 10:14:29 +0530

    Remove prompt(), add dynamic user input, event handling

--- bundle.js
+++ bundle.js
@@ -1,13 +1,13 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const Phrase = require("harbinger-complex-palindrome-detector");
 
-function palindromeTester() {
-    const userInput = prompt("Enter a string to check palindromicity: ", "abba");
+function palindromeTester(event) {
+    event.preventDefault();
     
-    let phrase = new Phrase(userInput);
+    let phrase = new Phrase(event.target.phrase.value);
     let palindromeResult = document.querySelector("#palindromeResult");
 
-    if(!!userInput  && phrase.isPalindrome() === true) {
+    if(phrase.isPalindrome() === true) {
         palindromeResult.innerHTML = `<strong>"${phrase.content}"</strong> is a palindrome`;    
     } else {
         palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome!!`;
@@ -16,8 +16,8 @@ function palindromeTester() {
 
 document.addEventListener("DOMContentLoaded", function () {
     let form = document.querySelector("#palindromeTester");
-    form.addEventListener("click", function () {
-        palindromeTester();
+    form.addEventListener("submit", function () {
+        palindromeTester(event);
     });
 });
 
--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const Phrase = require("harbinger-complex-palindrome-detector");
 
-function palindromeTester() {
-    const userInput = prompt("Enter a string to check palindromicity: ", "abba");
+function palindromeTester(event) {
+    event.preventDefault();
     
-    let phrase = new Phrase(userInput);
+    let phrase = new Phrase(event.target.phrase.value);
     let palindromeResult = document.querySelector("#palindromeResult");
 
-    if(!!userInput  && phrase.isPalindrome() === true) {
+    if(phrase.isPalindrome() === true) {
         palindromeResult.innerHTML = `<strong>"${phrase.content}"</strong> is a palindrome`;    
     } else {
         palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome!!`;
@@ -15,7 +15,7 @@ function palindromeTester() {
 
 document.addEventListener("DOMContentLoaded", function () {
     let form = document.querySelector("#palindromeTester");
-    form.addEventListener("click", function () {
-        palindromeTester();
+    form.addEventListener("submit", function () {
+        palindromeTester(event);
     });
 });
